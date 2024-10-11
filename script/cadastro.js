39c97448d4452efc318ec47c9cacde2d3945e063
HEITOR HILLESHEIM DOS SANTOS <heitor_h_santos@estudante.sesisenai.org.br>
2024-10-11 11:58:47 -0300

    cadastro (localstorage)

---
+++ script/cadastro.js
@@ -0,0 +1,29 @@
+function cadastrarUsuario(){
+    let user = document.getElementById("user").value
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+    let confimarsenha= document.getElementById("confirmar-senha").value
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
+    
+    if (senha!=confimarsenha){
+        document.getElementById("erro").innerHTML="senhas incompatíveis"
+        return
+    }
+    
+    let usuarioExistente = usuarios.find(usuario =>usuario.email===email)
+    if (usuarioExistente){
+        document.getElementById("erro").innerHTML="email cadastrado"
+        return
+    }
+    let novoUser = {
+        id: Date.now(),
+        email: email,
+        user: user,
+        senha: btoa(senha),
+        playlists:[],
+
+    }
+    usuarios.push(novoUser)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+    window.location.href = "login.html"
+}
